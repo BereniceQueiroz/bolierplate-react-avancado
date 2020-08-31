65c8f3918033b2dbd9450e91903c3999a438c550
BereniceQueiroz <berenice_queiroz@yahoo.com.br>
2020-08-31 17:30:20 -0300

    chore: add husk e lint-staged

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,7 @@ export default function Home() {
 				<h1 className={styles.title}>
 					{' '}
 					Welcome to
-					<a href="https://nextjs.org">
-						Next.js!hshshshhshshshuwhduewhduwuwuwwuuwuwuwuwuuwuwuwuwuwuuwuwuwuwuwuwuwu
-					</a>
+					<a href="https://nextjs.org">Next.js!</a>
 				</h1>
 				<p className={styles.description}>
 					Get started by editing{' '}
